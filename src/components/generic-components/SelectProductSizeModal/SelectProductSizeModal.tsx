2627a3f510df3776d95a6526d448c5fd0c6ed908
Zimbru Florin <florinz@cicadatech.eu>
2024-12-02 23:58:04 +0200

    commit

---
+++ src/components/generic-components/SelectProductSizeModal/SelectProductSizeModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectProductSizeModal: React.FC = ({isOpen, onClose}) => {
+  return <div>SelectProductSizeModal</div>;
+};
+
+export default SelectProductSizeModal;
